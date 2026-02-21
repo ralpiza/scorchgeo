f90af8d6caa481d9d03564e75697642d793132d4
ralpiza <ralpiza23@gmail.com>
2026-02-20 22:35:48 -0800

    added map functionality

--- scorchgeo/__init__.py
+++ scorchgeo/__init__.py
@@ -3,3 +3,5 @@
 __author__ = """Rafael Alpizar"""
 __email__ = "ralpiza23@gmail.com"
 __version__ = "0.0.1"
+
+from .scorchgeo import *
--- scorchgeo/scorchgeo.py
+++ scorchgeo/scorchgeo.py
@@ -1,1 +1,15 @@
 """Main module."""
+
+import ipyleaflet
+
+class Map(ipyleaflet.Map):
+    def __init__(self, center=[0, 0], zoom=2, height="600px", **kwargs):
+        super().__init__(center=center, zoom=zoom, **kwargs)
+        self.add_control(ipyleaflet.LayersControl())
+        self.layout.height = height
+
+    def add_basemap(self, basemap = "OpenTopoMap"):
+        
+        url = eval(f"ipyleaflet.basemaps.{basemap}").build_url()
+        layer = ipyleaflet.TileLayer(url=url, name=basemap)
+        self.add_layer(layer)
